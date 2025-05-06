027c9fabc4d8cf78517c150f3ede901a58f42a7a
M7mdAbolila <m7md.abolila.cs1@gmail.com>
2025-05-06 14:36:43 +0300

    finish add logs form

--- lib/features/medical_history/presentation/screens/add_logs_screen.dart
+++ lib/features/medical_history/presentation/screens/add_logs_screen.dart
@@ -1,17 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '../../../../core/helpers/spacing.dart';
+import '../../../../core/widgets/custom_button.dart';
+import '../../../../core/widgets/custom_date_picker_widget.dart';
 import '../../../../core/widgets/custom_screen_app_bar.dart';
+import '../../../../core/widgets/custom_text_form_field.dart';
+import '../widgets/upload_report_widget.dart';
 
 class AddLogsScreen extends StatelessWidget {
   const AddLogsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [
-          CustomScreenAppBar(title: 'Logs'),
-        ],
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        body: Column(
+          children: [
+            const CustomScreenAppBar(title: 'Logs'),
+            Expanded(
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.w),
+                  child: Column(
+                    children: [
+                      verticalSpace(24),
+                      CustomDatePickerWidget(
+                        title: 'Log Date',
+                        controller: TextEditingController(),
+                      ),
+                      verticalSpace(16),
+                      CustomTextFormField(
+                        title: 'Log Type',
+                        hintText: 'Type Here..',
+                        controller: TextEditingController(),
+                        textInputAction: TextInputAction.next,
+                      ),
+                      verticalSpace(16),
+                      CustomTextFormField(
+                        title: 'Specialty',
+                        hintText: 'Type Here..',
+                        controller: TextEditingController(),
+                        textInputAction: TextInputAction.next,
+                      ),
+                      verticalSpace(16),
+                      CustomTextFormField(
+                        title: 'Value',
+                        hintText: 'Type Here..',
+                        controller: TextEditingController(),
+                        textInputAction: TextInputAction.next,
+                      ),
+                      verticalSpace(16),
+                      CustomTextFormField(
+                        title: 'Notes',
+                        hintText: 'Type here any notes..',
+                        maxLines: 7,
+                        controller: TextEditingController(),
+                        textInputAction: TextInputAction.next,
+                      ),
+                      verticalSpace(16),
+                      const UploadAReportWidget(),
+                      verticalSpace(32),
+                      CustomButton(
+                        title: 'Save',
+                        onTap: () {},
+                      ),
+                      verticalSpace(100),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
