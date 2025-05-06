b238f8fa4d99b2f5c3f0a3a1a145f7924e778648
M7mdAbolila <m7md.abolila.cs1@gmail.com>
2025-05-06 14:32:03 +0300

    finish x ray and surgery forms

--- lib/features/medical_history/presentation/screens/add_surgery_screen.dart
+++ lib/features/medical_history/presentation/screens/add_surgery_screen.dart
@@ -1,17 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '../../../../core/helpers/spacing.dart';
+import '../../../../core/widgets/custom_button.dart';
+import '../../../../core/widgets/custom_date_picker_widget.dart';
 import '../../../../core/widgets/custom_screen_app_bar.dart';
+import '../../../../core/widgets/custom_text_form_field.dart';
+import '../widgets/upload_report_widget.dart';
 
 class AddSurgeryScreen extends StatelessWidget {
   const AddSurgeryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [
-          CustomScreenAppBar(title: 'Surgery'),
-        ],
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        body: Column(
+          children: [
+            const CustomScreenAppBar(title: 'Surgery'),
+            Expanded(
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.w),
+                  child: Column(
+                    children: [
+                      verticalSpace(24),
+                      CustomDatePickerWidget(
+                        title: 'Surgery Date',
+                        controller: TextEditingController(),
+                      ),
+                      verticalSpace(16),
+                      CustomTextFormField(
+                        title: 'Specialty',
+                        hintText: 'Type Here..',
+                        controller: TextEditingController(),
+                        textInputAction: TextInputAction.next,
+                      ),
+                      verticalSpace(16),
+                      CustomTextFormField(
+                        title: 'Procedure Name',
+                        hintText: 'Type Here..',
+                        controller: TextEditingController(),
+                        textInputAction: TextInputAction.next,
+                      ),
+                      verticalSpace(16),
+                      CustomTextFormField(
+                        title: 'Surgeon Name',
+                        hintText: 'Type Here..',
+                        controller: TextEditingController(),
+                        textInputAction: TextInputAction.next,
+                      ),
+                      verticalSpace(16),
+                      CustomTextFormField(
+                        title: 'Clinic/ Hospital Name',
+                        hintText: 'Type Here..',
+                        controller: TextEditingController(),
+                        textInputAction: TextInputAction.next,
+                      ),
+                      verticalSpace(16),
+                      CustomTextFormField(
+                        title: 'Notes',
+                        hintText: 'Type here any notes..',
+                        maxLines: 7,
+                        controller: TextEditingController(),
+                        textInputAction: TextInputAction.next,
+                      ),
+                      verticalSpace(16),
+                      const UploadAReportWidget(),
+                      verticalSpace(32),
+                      CustomButton(
+                        title: 'Save',
+                        onTap: () {},
+                      ),
+                      verticalSpace(100),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/features/medical_history/presentation/screens/add_x_ray_screen.dart
+++ lib/features/medical_history/presentation/screens/add_x_ray_screen.dart
@@ -1,17 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:healix/features/medical_history/presentation/widgets/upload_report_widget.dart';
 
+import '../../../../core/helpers/spacing.dart';
+import '../../../../core/widgets/custom_button.dart';
+import '../../../../core/widgets/custom_date_picker_widget.dart';
 import '../../../../core/widgets/custom_screen_app_bar.dart';
+import '../../../../core/widgets/custom_text_form_field.dart';
 
 class AddXRayScreen extends StatelessWidget {
   const AddXRayScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [
-          CustomScreenAppBar(title: 'X-Ray/ECG'),
-        ],
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        body: Column(
+          children: [
+            const CustomScreenAppBar(title: 'X-Ray/ECG'),
+            Expanded(
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.w),
+                  child: Column(
+                    children: [
+                      verticalSpace(24),
+                      CustomDatePickerWidget(
+                        title: 'Scan Date',
+                        controller: TextEditingController(),
+                      ),
+                      verticalSpace(16),
+                      CustomTextFormField(
+                        title: 'Scan Type',
+                        hintText: 'Type Here..',
+                        controller: TextEditingController(),
+                        textInputAction: TextInputAction.next,
+                      ),
+                      verticalSpace(16),
+                      CustomTextFormField(
+                        title: 'Scan Name',
+                        hintText: 'Type Here..',
+                        controller: TextEditingController(),
+                        textInputAction: TextInputAction.next,
+                      ),
+                      verticalSpace(16),
+                      CustomTextFormField(
+                        title: 'Facility Name',
+                        hintText: 'Type Here..',
+                        controller: TextEditingController(),
+                        textInputAction: TextInputAction.next,
+                      ),
+                      verticalSpace(16),
+                      CustomTextFormField(
+                        title: 'Scanned Part',
+                        hintText: 'Type Here..',
+                        controller: TextEditingController(),
+                        textInputAction: TextInputAction.next,
+                      ),
+                      verticalSpace(16),
+                      CustomTextFormField(
+                        title: 'Notes',
+                        hintText: 'Type here any notes..',
+                        maxLines: 7,
+                        controller: TextEditingController(),
+                        textInputAction: TextInputAction.next,
+                      ),
+                      verticalSpace(16),
+                      const UploadAReportWidget(),
+                      verticalSpace(32),
+                      CustomButton(
+                        title: 'Save',
+                        onTap: () {},
+                      ),
+                      verticalSpace(100),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
